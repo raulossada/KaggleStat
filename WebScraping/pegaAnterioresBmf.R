a963cfe6754fb9f5d002e632f94e87a726f0bd6b
raulossada <raulossada@gmail.com>
2017-07-21 00:58:37 -0300

    pega os anteriores. tá quase pronto

--- WebScraping/pegaAnterioresBmf.R
+++ WebScraping/pegaAnterioresBmf.R
@@ -35,8 +35,8 @@ zbrowser$navigate(zUrl);
 
 #############################################################################
 
-listaDatas <- seq(from=as.Date("03/07/2017", format="%d/%m/%Y"), 
-                  to=as.Date("07/07/2017", format="%d/%m/%Y"), 
+listaDatas <- seq(from=as.Date("07/07/2017", format="%d/%m/%Y"), 
+                  to=as.Date("10/07/2017", format="%d/%m/%Y"), 
                   by="days");
 listaDatas <- format(x=listaDatas, "%d/%m/%Y");
 
@@ -69,114 +69,112 @@ for(dataEscolhida in listaDatas){
   
   # Pega a versão compactada dos dados XML da página
   
-  erro <- tryCatch({
-    webTabelaDadosAjustes <- zbrowser$findElement(using="id", value="tblDadosAjustes")
-  }, warning=function(w){
-    erro <- 2;
+  tryCatch({
+    suppressMessages({
+      webTabelaDadosAjustes <- zbrowser$findElement(using="id", value="tblDadosAjustes");
+      
+      htmlTabelaDadosAjustes <- webTabelaDadosAjustes$getElementAttribute("outerHTML")[[1]];
+      
+      #############################################################################
+      
+      # Pega o XML da pagina
+      xmlTabelaDadosAjustes <- xmlTreeParse(file=htmlTabelaDadosAjustes, useInternalNodes=T);
+      
+      
+      atualizadoEm3 <- strsplit(x=atualizadoEm, "/");
+      atualizadoEm3 <- unlist(atualizadoEm3);
+      atualizadoEm3 <- paste( atualizadoEm3[3], "_", atualizadoEm3[2], "_", 
+                              atualizadoEm3[1], sep="" );
+      
+      
+      atualizadoEm2 <- gsub(pattern="/", replacement="_", x=atualizadoEm);
+      arquivoNomeXml <- paste("tabelaXML_", atualizadoEm2, ".xml", sep="");
+      
+      saveXML(doc=xmlTabelaDadosAjustes, file=arquivoNomeXml);
+      
+      rm( list=c("webTabelaDadosAjustes", "htmlTabelaDadosAjustes", "xmlTabelaDadosAjustes") );
+      
+      #############################################################################
+      
+      # Le o arquivo xml. Foi o jeitinho que eu achei de transformar o xml em char
+      thepage <- readLines(con=arquivoNomeXml);
+      
+      if( file.exists(arquivoNomeXml)==TRUE ){
+        file.remove(arquivoNomeXml);
+      }
+      
+      rm( list=c("arquivoNomeXml") );
+      
+      #############################################################################
+      
+      # Pega os nomes das colunas da tabela
+      webTitulos <- grep(pattern="<th ", x=thepage, value=TRUE);
+      titulos <- sub(pattern=".*\">", replacement="", x=webTitulos);
+      titulos <- sub(pattern="</th>$", replacement="", x=titulos);
+      titulos <- c(titulos, "Data Atualização");
+      
+      titulos <- html2char(titulos);
+      
+      rm( list=c("webTitulos") );
+      
+      #############################################################################
+      
+      # Separa a tabela HTML
+      webTabela <- grep(pattern="<td", x=thepage, value=TRUE);
+      # Cria uma tabela com os dados
+      tabelaDados <- NULL;
+      ii <- 1;
+      while(ii < length(webTabela) ){
+        tagMercadoria <- sub(pattern=".*<td ", replacement="", x=webTabela[ii]);
+        tagMercadoria <- sub(pattern="=.*$", replacement="", x=tagMercadoria);
+        
+        candidataMercadoria <- sub(pattern=".*>(.*?)", replacement="", x=webTabela[ii]);
+        candidataMercadoria <- sub(pattern=" *</td>$", replacement="", x=candidataMercadoria);
+        
+        if(tagMercadoria!="class"){
+          Mercadoria <- candidataMercadoria;
+        }else{
+          ii <- ii-1;
+        }
+        
+        webLinhaDados <- webTabela[(ii+1):(ii+5)];
+        LinhaDados <- sub(pattern=".*\">", replacement="", x=webLinhaDados);
+        LinhaDados <- sub(pattern=" </td>$", replacement="", x=LinhaDados);
+        LinhaDados <- sub(pattern="</td>$", replacement="", x=LinhaDados);
+        LinhaDados <- data.frame( t( c(Mercadoria, LinhaDados) ) );
+        
+        tabelaDados <- rbind(tabelaDados, LinhaDados);
+        ii <- ii+6;
+      }
+      
+      tabelaDados <- cbind(tabelaDados, atualizadoEm);
+      
+      names(tabelaDados) <- titulos;
+      
+      tabelaDados$Mercadoria <- html2char(tabelaDados$Mercadoria);
+      
+      rm( list=c("atualizadoEm", "LinhaDados", "candidataMercadoria", "ii", "Mercadoria", 
+                 "thepage", "tagMercadoria", "titulos", "webLinhaDados", "webTabela") );
+      
+      #############################################################################
+      
+      tabelaDados2 <- tabelaDados;
+      tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=as.character );
+      tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=charComma2dot );
+      tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=as.numeric );
+      
+      arquivoNomeCsv <- paste("derivativos_ajuste_", atualizadoEm3, ".csv", sep="");
+      
+      if( file.exists(arquivoNomeCsv)==FALSE ){
+        write.csv2(x=tabelaDados2, file=arquivoNomeCsv, row.names=FALSE);
+      }
+      
+      rm( list=c("tabelaDados", "tabelaDados2", "arquivoNomeCsv", 
+                 "atualizadoEm2", "atualizadoEm3") );
+    })
   }, error=function(e){
-    erro <- 3;
-  }, finally={
-    erro <- 4;
+    erro <- 1;
   });
-  
-  htmlTabelaDadosAjustes <- webTabelaDadosAjustes$getElementAttribute("outerHTML")[[1]];
-  
-  #############################################################################
-  
-  # Pega o XML da pagina
-  xmlTabelaDadosAjustes <- xmlTreeParse(file=htmlTabelaDadosAjustes, useInternalNodes=T);
-  
-  
-  atualizadoEm3 <- strsplit(x=atualizadoEm, "/");
-  atualizadoEm3 <- unlist(atualizadoEm3);
-  atualizadoEm3 <- paste( atualizadoEm3[3], "_", atualizadoEm3[2], "_", 
-                          atualizadoEm3[1], sep="" );
-  
-  
-  atualizadoEm2 <- gsub(pattern="/", replacement="_", x=atualizadoEm);
-  arquivoNomeXml <- paste("tabelaXML_", atualizadoEm2, ".xml", sep="");
-  
-  saveXML(doc=xmlTabelaDadosAjustes, file=arquivoNomeXml);
-  
-  rm( list=c("webTabelaDadosAjustes", "htmlTabelaDadosAjustes", "xmlTabelaDadosAjustes") );
-  
-  #############################################################################
-  
-  # Le o arquivo xml. Foi o jeitinho que eu achei de transformar o xml em char
-  thepage <- readLines(con=arquivoNomeXml);
-  
-  if( file.exists(arquivoNomeXml)==TRUE ){
-    file.remove(arquivoNomeXml);
-  }
-  
-  rm( list=c("arquivoNomeXml") );
-  
-  #############################################################################
-  
-  # Pega os nomes das colunas da tabela
-  webTitulos <- grep(pattern="<th ", x=thepage, value=TRUE);
-  titulos <- sub(pattern=".*\">", replacement="", x=webTitulos);
-  titulos <- sub(pattern="</th>$", replacement="", x=titulos);
-  titulos <- c(titulos, "Data Atualização");
-  
-  titulos <- html2char(titulos);
-  
-  rm( list=c("webTitulos") );
-  
-  #############################################################################
-  
-  # Separa a tabela HTML
-  webTabela <- grep(pattern="<td", x=thepage, value=TRUE);
-  # Cria uma tabela com os dados
-  tabelaDados <- NULL;
-  ii <- 1;
-  while(ii < length(webTabela) ){
-    tagMercadoria <- sub(pattern=".*<td ", replacement="", x=webTabela[ii]);
-    tagMercadoria <- sub(pattern="=.*$", replacement="", x=tagMercadoria);
-    
-    candidataMercadoria <- sub(pattern=".*>(.*?)", replacement="", x=webTabela[ii]);
-    candidataMercadoria <- sub(pattern=" *</td>$", replacement="", x=candidataMercadoria);
-    
-    if(tagMercadoria!="class"){
-      Mercadoria <- candidataMercadoria;
-    }else{
-      ii <- ii-1;
-    }
-    
-    webLinhaDados <- webTabela[(ii+1):(ii+5)];
-    LinhaDados <- sub(pattern=".*\">", replacement="", x=webLinhaDados);
-    LinhaDados <- sub(pattern=" </td>$", replacement="", x=LinhaDados);
-    LinhaDados <- sub(pattern="</td>$", replacement="", x=LinhaDados);
-    LinhaDados <- data.frame( t( c(Mercadoria, LinhaDados) ) );
-    
-    tabelaDados <- rbind(tabelaDados, LinhaDados);
-    ii <- ii+6;
-  }
-  
-  tabelaDados <- cbind(tabelaDados, atualizadoEm);
-  
-  names(tabelaDados) <- titulos;
-  
-  tabelaDados$Mercadoria <- html2char(tabelaDados$Mercadoria);
-  
-  rm( list=c("atualizadoEm", "LinhaDados", "candidataMercadoria", "ii", "Mercadoria", 
-             "thepage", "tagMercadoria", "titulos", "webLinhaDados", "webTabela") );
-  
-  #############################################################################
-  
-  tabelaDados2 <- tabelaDados;
-  tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=as.character );
-  tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=charComma2dot );
-  tabelaDados2[, 3:6] <- apply( X=tabelaDados2[, 3:6], MARGIN=2, FUN=as.numeric );
-  
-  arquivoNomeCsv <- paste("derivativos_ajuste_", atualizadoEm3, ".csv", sep="");
-  
-  if( file.exists(arquivoNomeCsv)==FALSE ){
-    write.csv2(x=tabelaDados2, file=arquivoNomeCsv, row.names=FALSE);
-  }
-  
-  rm( list=c("tabelaDados", "tabelaDados2", "arquivoNomeCsv", 
-             "atualizadoEm2", "atualizadoEm3") );
 }
 
 # Fecha o navegador
