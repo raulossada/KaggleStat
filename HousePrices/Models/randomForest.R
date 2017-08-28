9c72cc2d9da9a11591e4797dd97fb8ebc22e08ac
lucashamaguchi <lucas_hamaguchi@hotmail.com>
2017-08-28 17:26:53 -0300

    Comment

--- HousePrices/Models/randomForest.R
+++ HousePrices/Models/randomForest.R
@@ -3,7 +3,7 @@
 #Random Forest
 install.packages('randomForest')
 library(randomForest)
-
+set.seed(1231) #Não sei ainda porque (rever)
 regressor = randomForest(x = dataframe_of_independent_variables,
                          y = vector_of_dependent_variable,
                          ntrees = number_of_trees)
