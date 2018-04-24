0663b89e30bd4cc871c0221b3b4c87eabdd55acd
lucashamaguchi <lucas_hamaguchi@hotmail.com>
2018-04-23 23:52:07 -0300

    Arrumando a matriz de correlação para rodar Começando de fato com o RandomForest

--- HousePrices/Models/randomForest.R
+++ HousePrices/Models/randomForest.R
@@ -1,14 +1,33 @@
 #Testes de modelos
+#Dê um setwd na pasta HousePrices, onde quer que ela esteja em sua máquina
 
+training_data <- read.csv(file="Data/train.csv", as.is=FALSE);
+remover_colunas <- c("Id"); 
+training_data <- training_data[ , !(names(training_data) %in% remover_colunas ) ];
 
 #Primeiro desafio: Escolher as váriaveis explicativas para usar
 #Espaço para a escolha kk
 
 
 #Random Forest
-install.packages('randomForest')
-library(randomForest)
+
+##Pacotes que serão utilizados
+##incluir em libs novos pacotes
+libs = c('randomForest');
+for(ii in libs){
+  if(!( ii %in% installed.packages())){
+    install.packages(ii);
+  }
+  library(ii, character.only=TRUE );
+}
+
+dataframe_of_independent_variables = training_data[ , !(names(training_data) %in% c('SalePrice')) ]
+vector_of_dependent_variable  = training_data[ , names(training_data) == 'SalePrice']
+number_of_trees = 500
+
 set.seed(1231) #Não sei ainda porque (rever)
 regressor = randomForest(x = dataframe_of_independent_variables,
                          y = vector_of_dependent_variable,
                          ntrees = number_of_trees)
+
+
